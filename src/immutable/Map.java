bd2352947dbe0cfb87b57adba02dc1df44bb2248
Eagan <eegykennedy@gmail.com>
2025-11-26 13:14:33 -0600

    Map interface and implementation init complete

--- src/immutable/Map.java
+++ src/immutable/Map.java
@@ -1,4 +1,22 @@
 package immutable;
 
+import java.util.List;
+
 public interface Map {
+    public interface Entry{
+        public Object getKey();
+        public Object getValue();
+    }
+
+    public static Map create(){
+        return null;
+    }
+
+    public abstract int size();
+    public abstract int depth();
+    public abstract boolean empty();
+    public abstract Map setValue(Object key, Object value);
+    public abstract Object getValue(Object key);
+    public List<Entry> getEntries();
+    //move to implement when possible. protected abstract void addEntries()
 }
---
+++ src/immutable/Node.java
@@ -0,0 +1,30 @@
+package immutable;
+
+import java.util.List;
+
+public class Node implements Map{
+    @Override
+    public int size() {
+        return 0;
+    }
+    @Override
+    public int depth() {
+        return 0;
+    }
+    @Override
+    public boolean empty() {
+        return false;
+    }
+    @Override
+    public Map setValue(Object key, Object value) {
+        return null;
+    }
+    @Override
+    public Object getValue(Object key) {
+        return null;
+    }
+    @Override
+    public List<Entry> getEntries() {
+        return List.of();
+    }
+}
